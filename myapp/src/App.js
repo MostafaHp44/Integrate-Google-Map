b2a378bb48445ac1e22732c793df7744d72cdcc8
mostafa <mostafahp44@gmail.com>
2023-02-07 20:24:19 +0200

    'done'

---
+++ myapp/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+
+     <div className="bigdiv">
+    <iframe src= 'https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Cairo+(MooMap)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed'>
+       <a href="https://www.maps.ie/distance-area-calculator.html">distance maps</a>
+    </iframe>
+    </div>
+
+    </div>
+  );
+}
+
+export default App;
